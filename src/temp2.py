85aea29130b7d06d786dc7dad5d2e2559bd73453
CodeJxer <zhixinl@mails.ccnu.edu.cn>
2018-01-23 23:15:21 +0800

    show the wave picture of the mp3

---
+++ src/temp2.py
@@ -0,0 +1,27 @@
+#import
+import wave as we
+import numpy as np
+import matplotlib.pyplot as plt
+
+def wavread(path):
+    wavfile =  we.open(path,"rb")
+    params = wavfile.getparams()
+    framesra,frameswav= params[2],params[3]
+    datawav = wavfile.readframes(frameswav)
+    wavfile.close()
+    datause = np.fromstring(datawav,dtype = np.short)
+    datause.shape = -1,2
+    datause = datause.T
+    time = np.arange(0, frameswav) * (1.0/framesra)
+    return datause,time
+
+def main():
+    wavdata, wavtime = wavread('test.wav')
+    plt.title("test.wav's Frames")
+    plt.subplot(211)
+    plt.plot(wavtime, wavdata[0],color = 'green')
+    plt.subplot(212)
+    plt.plot(wavtime, wavdata[1])
+    plt.show()
+    
+main()
